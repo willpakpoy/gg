76310a7bbd95db2b1004ca8773b8cabc1e13e519
Will Pak Poy <0521858@radford.act.edu.au>
2021-05-27 07:36:58 +1000

    damn look at that slick css animation

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,6 +1,7 @@
 
 function displayMobileMenu() {
-  document.querySelector(".mobile-menu").classList.toggle("open")
+  document.querySelector(".mobile-menu").classList.toggle("open");
+  document.querySelector(".nav-links-outer").classList.toggle("open");
 }
 
 document.querySelector(".mobile-menu").addEventListener("click", displayMobileMenu); 
