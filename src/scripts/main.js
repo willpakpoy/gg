eb167646c53df86382863a3527bfc8443a7cff80
Will Pak Py (RA UBD 2) <0521858@radford.act.edu.au>
2021-05-28 15:19:49 +1000

    drop most files

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-import "../styles/universal.scss";
-
-import "./header";
-
-window.addEventListener('scroll', () => {
-    var scrollHeight = window.pageYOffset / (document.querySelector("header").offsetHeight - window.innerHeight)
-    var scrollHeightPercentage = scrollHeight > -1 ? scrollHeight : -1
-    document.documentElement.style.setProperty('--scroll', Math.abs(scrollHeightPercentage));
-  }, false);
