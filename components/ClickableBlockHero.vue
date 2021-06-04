1fda2998d6bacb3ff288a83efe8443c3a3556221
Will Pak Poy (Mac) <0521858@radford.act.edu.au>
2021-06-04 23:30:09 +1000

    homepage is done

---
+++ components/ClickableBlockHero.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="clickable-block-hero" @click="clicked()">
+    <div class="image" :style="{'background-image': `url(${image})`}"/>
+    <div class="content-inner">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'ClickableBlockHero',
+    props: [
+        'image',
+        'linkTo',
+    ],
+    methods: {
+        clicked() {
+            this.$router.push(this.linkTo);
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.clickable-block-hero {
+  display: flex;
+  position: relative;
+
+  .image {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  }
+
+  .content-inner {
+    opacity: 0;
+    color: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    transition: 0.2s ease-in-out;
+  }
+
+  &:hover {
+    .content-inner {
+      transition: 0.2s ease-in-out;
+      backdrop-filter: brightness(0.2) blur(4px);
+      opacity: 1;
+      cursor: pointer;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,52 @@
 <template>
+<main>
   <full-width-image-hero :image="require(`@/assets/images/pexels/mcf-home-sunrise.jpg`)" >
     <h1>Welcome to<br>The Mim Chapman Foundation</h1>
     <h2>Igniting creativity through continuous learning</h2>
   </full-width-image-hero>
+
+  <div class="home-grid">
+
+    <clickable-block-hero
+    linkTo="/who-we-are"
+    :image="require(`@/assets/images/pexels/mcf-home-tiles-who-we-are.jpeg`)"
+    >
+      <h1>who we are</h1>
+    </clickable-block-hero>
+
+    <clickable-block-hero
+    linkTo="/what-we-do"
+    :image="require(`@/assets/images/pexels/mcf-home-tiles-what-we-do.jpeg`)"
+    >
+      <h1>what we do</h1>
+    </clickable-block-hero>
+
+    <clickable-block-hero
+    linkTo="/our-stories"
+    :image="require(`@/assets/images/pexels/mcf-home-tiles-our-stories.jpeg`)"
+    >
+      <h1>our stories</h1>
+    </clickable-block-hero>
+
+    <clickable-block-hero
+    linkTo="/get-involved"
+    :image="require(`@/assets/images/pexels/mcf-home-tiles-get-involved.jpeg`)"
+    >
+      <h1>get involved</h1>
+    </clickable-block-hero>
+
+  </div>
+</main>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style scoped lang="scss">
+.home-grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 40vh);
+}
+</style>
